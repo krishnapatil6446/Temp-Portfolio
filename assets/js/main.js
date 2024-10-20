63180e02c8973dd608e27d896dd7530e2d264c12
Krishna Patil <krishnapatil6446@gmail.com>
2024-10-20 09:48:24 +0530

    some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["FullStack Developer","","Developer"],
+  strings : ["FullStack Developer","Native App Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
